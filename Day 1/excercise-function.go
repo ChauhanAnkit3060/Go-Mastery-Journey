f4df1c05c5a169937dffd4f1a5ba686b13964b82
ChauhanAnkit3060 <theankitchauhan3060@gmail.com>
2025-11-22 20:46:14 +0530

    Added some extra code

---
+++ Day 1/excercise-function.go
@@ -0,0 +1,7 @@
+package main
+
+const PI = 3.14159
+
+func calcCircumference(Diameter float64) float64{
+	return PI * Diameter
+}
--- Day 1/main.go
+++ Day 1/main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
 func main() {
 	fmt.Println("Hello Git...")
 	fmt.Printf("Time now : %v",time.Now())
+
+	var dia_txt string	
+	fmt.Print("\nEnter diameter of circle : ")	
+	fmt.Scanln(&dia_txt) 
+	fmt.Println("Entered diameter : ",dia_txt)
+	dia , err := strconv.ParseFloat(dia_txt,64)
+	if err != nil{
+		fmt.Println("Invalid response")
+		return
+	}
+	fmt.Printf("Circumference : %.2f\n",calcCircumference(dia))
+	// The '&' before 'name' passes the memory address of the variable	
+	// fmt.Printf("Hello, %s!\n", name)	var age int	
+	// fmt.Print("Enter your age: ")	fmt.Scanln(&age)	
+	// fmt.Printf("You are %d years old.\n", age)}
+
+	reader := bufio.NewReader(os.Stdin) // Create a new reader for standard input	
+	fmt.Print("Enter a sentence: ")	
+	sentence, _ := reader.ReadString('\n') // Read until a newline character	
+	fmt.Printf("You entered: %s", sentence) // sentence will include the newline character}
 }
 
 //git config --global user.name "ChauhanAnkit3060"
 //git config --global user.email "theankitchauhan3060@gmail.com"
+
