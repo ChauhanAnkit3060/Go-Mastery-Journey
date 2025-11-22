0321e9f8fb63bc3d6c996198adf6634309e723e0
ChauhanAnkit3060 <theankitchauhan3060@gmail.com>
2025-11-22 18:40:13 +0530

    This is my first commit Added Day 1 in Go mastery with a simple program to print hello Git

---
+++ Day 1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Git...")
+}
+
+//git config --global user.name "ChauhanAnkit3060"
+//git config --global user.email "theankitchauhan3060@gmail.com"
