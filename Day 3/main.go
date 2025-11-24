e169f852563a8b1b148dd40c5bd21d74fe985c0b
ChauhanAnkit3060 <theankitchauhan3060@gmail.com>
2025-11-25 00:42:26 +0530

    Added day 3 program on error retrun

---
+++ Day 3/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func CheckPasswordStrength(pass string) (isValid bool, err error) {
+	isValid = true
+	err = nil
+	if len(pass) < 8 {
+		isValid = false
+		err = fmt.Errorf("password must be at least 8 char long")
+	}
+	return
+}
+
+func main() {
+	
+	fmt.Printf("Enter password : ")
+	reader := bufio.NewReader(os.Stdin)
+	pass, err := reader.ReadString('\n')
+	if err != nil{
+		log.Fatal(err)
+		return
+	}
+	
+
+	pass = strings.TrimSpace(pass)
+	fmt.Printf("%v is of length %d\n",pass,len(pass))
+	if isValid, err := CheckPasswordStrength(pass); !isValid{
+		log.Fatal(err)
+		return
+	}
+	fmt.Println("Password is valid")
+}
