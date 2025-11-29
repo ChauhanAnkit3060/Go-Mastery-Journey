7eb60251e11a689dd246a6e43709f8a26f58498a
ChauhanAnkit3060 <theankitchauhan3060@gmail.com>
2025-11-29 18:41:39 +0530

    Working on project

--- Projects/simple-cli-text-processing/main.go
+++ Projects/simple-cli-text-processing/main.go
@@ -1,31 +1,46 @@
 package main
 
 import (
-	"flag"
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
+func userInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+	if err != nil{
+		return "",fmt.Errorf("error while taking input from stdio : %v",err)
+	}
+	input = strings.TrimSpace(input)
+	return input, nil
+}
 
+func readFromFile(filename string) (string, error){
+	file,err := os.Open(filename)
+	if err != nil{
+		return "", fmt.Errorf("error while opening from file : %v",err)
+	}
+	defer file.Close()
+	data := make([]byte,1000)
+	n,err := file.Read(data)
+	if err == io.EOF{
+		return string(data),nil
+	}
+	if err != nil{
+		return "", fmt.Errorf("error while reading from file : %v",err)
+	}
+	return string(data[:n]),nil
+}
+
+func filePrompt(){
+	fmt.Print("Please select file operator (n - create) ")
+}
 
 func main() {
-	var readfile, writefile string
-	flag.StringVar(&readfile,"readfile","readfile.txt","file to read from !")
-	flag.StringVar(&writefile,"writefile","filewrite.txt","file to write in !")
-	flag.Parse()
-	fmt.Println(readfile,writefile)
-
-	data, err := os.ReadFile(readfile)
-	if err!=nil{
-		panic(err)
-	}
-	strdata := string(data)
-	strdata = strings.ReplaceAll(strdata,"\n","\n😈")
-	if err :=os.WriteFile(writefile,[]byte(strdata),0644);err!=nil{
-		panic(err)
-	}
-
-	fmt.Println("Program completed")
-	
+	reader := bufio.NewReader(os.Stdin)
+	name, _ := userInput("Enter file : ",reader)
+	fmt.Print(name)
 }
