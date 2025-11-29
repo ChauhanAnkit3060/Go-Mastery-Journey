7c28ff55bab8ed31a1ed27818691713f8169ea41
ChauhanAnkit3060 <theankitchauhan3060@gmail.com>
2025-11-29 20:16:08 +0530

    First darft of CLI project

--- Projects/simple-cli-text-processing/main.go
+++ Projects/simple-cli-text-processing/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -24,7 +26,7 @@ func readFromFile(filename string) (string, error){
 		return "", fmt.Errorf("error while opening from file : %v",err)
 	}
 	defer file.Close()
-	data := make([]byte,1000)
+	data := make([]byte,10000)
 	n,err := file.Read(data)
 	if err == io.EOF{
 		return string(data),nil
@@ -35,12 +37,74 @@ func readFromFile(filename string) (string, error){
 	return string(data[:n]),nil
 }
 
-func filePrompt(){
-	fmt.Print("Please select file operator (n - create) ")
+func writeTofile(content, filepath string) error{
+	file, err := os.Create(filepath)
+	if err != nil{
+		return fmt.Errorf("error while creating file %s : %v",filepath,err)
+	}
+	defer file.Close()
+	n, err := file.Write([]byte(content))
+	if err!=nil{
+		return fmt.Errorf("error writing to file %s : %v",filepath,err)
+	}
+	fmt.Printf("%d byte written to file:%s successfully\n",n,filepath)
+	return nil
+}
+
+func upperOrLower(str string, toUpper bool) string{
+	if toUpper{
+		return strings.ToUpper(str)
+	}
+	return strings.ToLower(str)
+}
+
+func strReverse(str string) string{
+	strArr := strings.Split(str, " ")
+	slices.Reverse(strArr)
+	str = strings.Join(strArr," ")
+	return str
+}
+
+func fileOption(readfile, writefile string, reader *bufio.Reader){
+	content,err := readFromFile(readfile)
+	if err!=nil{
+		panic(err)
+	}
+	opt, _ := userInput("Please select operation (r: reverse date | u: all caps | l:lower case) : ",reader)
+	switch opt{
+	case "r" :
+		content = strReverse(content)
+		if err := writeTofile(content,writefile); err!=nil{
+			panic(err)
+		}
+		fmt.Println("Contents of file are revsered")
+	case "u" :
+		content = upperOrLower(content,true)
+		if err := writeTofile(content,writefile); err!=nil{
+			panic(err)
+		}
+		fmt.Println("Contents of file are converted to uppercase")
+	case "l" :
+		content = upperOrLower(content,false)
+		if err := writeTofile(content,writefile); err!=nil{
+			panic(err)
+		}
+		fmt.Println("Contents of file are converted to uppercase")
+	default :
+		fmt.Println("Entered value is not valid ....")
+		fileOption(readfile,writefile,reader)
+	}
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	name, _ := userInput("Enter file : ",reader)
-	fmt.Print(name)
+	var readfile, writefile string
+	flag.StringVar(&readfile,"readfile","file.txt","File path to read from")
+	flag.StringVar(&writefile,"writefile","output.txt","File path to write into")
+	flag.Parse()
+
+	content,_ := readFromFile(readfile)
+	fmt.Printf("File: %s currently contains\n%s \n%s",readfile,strings.Repeat("-",50),content)
+	fmt.Println()
+	fileOption(readfile,writefile,reader)
 }
